b6350df5ddea84e8365841728ffb7e14ce5eb165
Joseph Hanrahan <jhanrahan@hpc003.nsls2.bnl.local>
2023-11-08 23:36:54 -0500

    implemented timepix end-run workflow

--- processing.py
+++ processing.py
@@ -6,6 +6,27 @@ tiled_client_chx = tiled_client["raw"]
 tiled_cilent_sandbox = tiled_client["sandbox"]
 tiled_cilent_processed = tiled_client["processed"]
 
+@task
+def get_df_uncent(run):
+    sid = run['start']['scan_id']
+    raw_file_paths = extract_fpaths_from_sid(sid)
+    for file in raw_file_paths:
+        if (os.path.exists(file)):
+            yield raw_to_sorted_df(file)
+
+@flow
+def insert_to_tiled(container, run):
+    structure = None
+    node = None
+    num_img = run['descriptors'][0]['configuration']['tpx3']['data']['tpx3_cam_num_images']
+
+    for partition_num, df in enumerate(get_df_uncent(run)):
+        if (structure == None):
+            structure = TableStructure.from_pandas(df)
+            structure.npartitions = num_img
+            node = container.new("table", structure=structure, metadata={"raw": run['start']['uid']})
+        
+        node.write_partition(df, partition_num)
 
 @task
 def process_run(ref):
@@ -21,11 +42,15 @@ def process_run(ref):
     logger = get_run_logger()
     # Grab the BlueSky run
     run = tiled_client_chx[ref]
+    
     # Grab the full uid for logging purposes
     full_uid = run.start["uid"]
     logger.info(f"{full_uid = }")
     logger.info("Do something with this uid")
-    # Do some additional processing or call otehr python processing functions
+    
+    client = from_uri('https://tiled.nsls2.bnl.gov')
+    container = client['chx']['processed']
+    insert_to_tiled(container, run)
 
 
 @flow
@@ -38,5 +63,7 @@ def processing_flow(ref):
     ref : int, str
         reference to BlueSky. It can be scan_id, uid or index
     """
-
-    process_run(ref)
+    run = tiled_client_chx[ref]
+    
+    if (run['start']['detectors'][0] == 'tpx3'):
+        process_run(ref)
