cebc6fbe661f13638d5c74b439e467e826d8c955
JGoodrichBNL <103213447+JGoodrichBNL@users.noreply.github.com>
2023-11-17 14:47:50 -0500

    Update processing.py

--- processing.py
+++ processing.py
@@ -1,5 +1,6 @@
 from prefect import flow, task, get_run_logger
 from tiled.client import from_profile
+from tpx3awkward import extract_fpaths_from_sid, raw_to_sorted_df
 
 tiled_client = from_profile("nsls2")["chx"]
 tiled_client_chx = tiled_client["raw"]
@@ -16,7 +17,7 @@ def get_df_uncent(run):
 def insert_to_tiled(container, run):
     structure = None
     node = None
-    num_img = run['descriptors'][0]['configuration']['tpx3']['data']['tpx3_cam_num_images']
+    num_img = run['primary'].metadata['descriptors'][0]['configuration']['tpx3']['data']['tpx3_cam_num_images']
 
     for partition_num, df in enumerate(get_df_uncent(run)):
         if (structure == None):
