628f165e3956432fc21d4b58ddc4465e0b1475fd
Joseph Hanrahan <jhanrahan@hpc004.nsls2.bnl.local>
2023-11-29 22:44:38 -0500

    imported Tiled TableStructure

--- processing.py
+++ processing.py
@@ -1,7 +1,6 @@
 from prefect import flow, task, get_run_logger
 from tiled.client import from_profile
-from tpx3utils import raw_to_sorted_df
-import os
+from tiled.structures.table import TableStructure
 
 tiled_client = from_profile("nsls2")["chx"]
 tiled_client_chx = tiled_client["raw"]
@@ -9,8 +8,8 @@ tiled_client_sandbox = tiled_client["sandbox"]
 tiled_client_processed = tiled_client["processed"]
 
 def get_df_uncent(run):
-    sid = run.start['scan_id']
-    raw_file_paths = list(run['primary']['data']['tpx3_files_raw_filepaths'][0])
+    sid = run['start']['scan_id']
+    raw_file_paths = extract_fpaths_from_sid(sid)
     for file in raw_file_paths:
         if (os.path.exists(file)):
             yield raw_to_sorted_df(file)
@@ -18,13 +17,13 @@ def get_df_uncent(run):
 def insert_to_tiled(container, run):
     structure = None
     node = None
-    num_img = run['primary'].metadata['descriptors'][0]['configuration']['tpx3']['data']['tpx3_cam_num_images']
+    num_img = run['descriptors'][0]['configuration']['tpx3']['data']['tpx3_cam_num_images']
 
     for partition_num, df in enumerate(get_df_uncent(run)):
         if (structure == None):
             structure = TableStructure.from_pandas(df)
             structure.npartitions = num_img
-            node = container.new("table", structure=structure, metadata={"raw": run.start['uid']})
+            node = container.new("table", structure=structure, metadata={"raw": run['start']['uid']})
         
         node.write_partition(df, partition_num)
 
@@ -48,7 +47,7 @@ def process_run(ref):
     logger.info(f"{full_uid = }")
     logger.info("Do something with this uid")
     
-    insert_to_tiled(tiled_client_sandbox, run)
+    insert_to_tiled(tiled_client_processed, run)
 
 
 @flow
@@ -63,5 +62,5 @@ def processing_flow(ref):
     """
     run = tiled_client_chx[ref]
     
-    if (run.start['detectors'][0] == 'tpx3'):
+    if (run['start']['detectors'][0] == 'tpx3'):
         process_run(ref)
